5ca2ec2567f78f69dd479c5a1ad044bcd2ad56f5
George Wood <sanugsanug@yahoo.com>
2018-06-01 17:03:50 -0500

    ACC Playground

---
+++ GWWTest.playground/Pages/TestPlayground.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground!"
+print(str, terminator: "")
+print(terminator: "\t")
+print ()
+print("George Wood's playground")
